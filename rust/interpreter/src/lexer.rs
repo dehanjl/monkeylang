d3e2f69c29aac8794890f0557b71c1a97baf9657
Dehan Jaco Lamprecht <dehanjl@pm.me>
2024-08-10 21:24:42 +0200

    Iterator based Lexer
    And some extra functions

--- rust/interpreter/src/lexer.rs
+++ rust/interpreter/src/lexer.rs
@@ -1,33 +1,64 @@
+use std::str::Chars;
+
 use crate::token::Token;
 
-struct Lexer {
+struct Lexer<'a> {
     input: String,
-    position: usize,
-    read_position: usize,
-    ch: char,
+    position: usize,      // current position in input (points to current char)
+    read_position: usize, // current reading position in input (after current char)
+    ch: char,             // current char under examination
+    chars: Chars<'a>,
 }
 
-impl Lexer {
-    fn new(input: String) -> Lexer {
+impl Lexer<'_> {
+    fn new(input: &str) -> Lexer<'_> {
         let mut l = Lexer {
-            input,
+            input: input.to_string(),
             position: 0,
             read_position: 0,
             ch: '\0',
+            chars: input.chars(),
         };
         l.read_char();
         l
     }
 
     fn read_char(&mut self) {
-        if let Some(ch) = self.input.chars().nth(self.read_position) {
-            self.ch = ch;
-        } else {
-            self.ch = '\0';
-        }
+        self.ch = self.chars.next().unwrap_or('\0');
 
         self.position = self.read_position;
         self.read_position += 1;
+    }
+
+    fn peek_char(&self) -> char {
+        self.chars
+            .clone()
+            .peekable()
+            .peek()
+            .cloned()
+            .unwrap_or('\0')
+    }
+
+    fn skip_whitespace(&mut self) {
+        while self.ch.is_whitespace() {
+            self.read_char();
+        }
+    }
+
+    fn read_identifier(&mut self) -> String {
+        let position = self.position;
+        while self.ch.is_ascii_alphabetic() || self.ch == '_' {
+            self.read_char();
+        }
+        self.input[position..self.position].to_string()
+    }
+
+    fn read_number(&mut self) -> String {
+        let position = self.position;
+        while self.ch.is_ascii_digit() {
+            self.read_char();
+        }
+        self.input[position..self.position].to_string()
     }
 
     fn next_token(&mut self) -> Option<Token> {
@@ -64,7 +95,7 @@ mod tests {
             Token::COMMA,
             Token::SEMICOLON,
         ];
-        let mut l = Lexer::new(input.to_string());
+        let mut l = Lexer::new(input);
 
         for (i, tt) in tests.iter().enumerate() {
             let tok = l.next_token().unwrap();
