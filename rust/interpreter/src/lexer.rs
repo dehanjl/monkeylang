2f6a57849e2eb590e4ff6cbe5db123a6d09eb784
Dehan Jaco Lamprecht <dehanjl@pm.me>
2024-08-10 19:07:28 +0200

    Simple tokenset and lexer in rust

--- rust/interpreter/src/lexer.rs
+++ rust/interpreter/src/lexer.rs
@@ -1,7 +1,77 @@
+use crate::token::Token;
+
+struct Lexer {
+    input: String,
+    position: usize,
+    read_position: usize,
+    ch: char,
+}
+
+impl Lexer {
+    fn new(input: String) -> Lexer {
+        let mut l = Lexer {
+            input,
+            position: 0,
+            read_position: 0,
+            ch: '\0',
+        };
+        l.read_char();
+        l
+    }
+
+    fn read_char(&mut self) {
+        if let Some(ch) = self.input.chars().nth(self.read_position) {
+            self.ch = ch;
+        } else {
+            self.ch = '\0';
+        }
+
+        self.position = self.read_position;
+        self.read_position += 1;
+    }
+
+    fn next_token(&mut self) -> Option<Token> {
+        let tok = match self.ch {
+            '=' => Token::ASSIGN,
+            '+' => Token::PLUS,
+            '(' => Token::LPAREN,
+            ')' => Token::RPAREN,
+            '{' => Token::LBRACE,
+            '}' => Token::RBRACE,
+            ',' => Token::COMMA,
+            ';' => Token::SEMICOLON,
+            '\0' => return None,
+            _ => Token::ILLEGAL,
+        };
+        self.read_char();
+        Some(tok)
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
     #[test]
-    fn test_token() {
-        assert_eq!(2 + 2, 4);
+    fn test_next_token_simple() {
+        let input = "=+(){},;";
+        let tests = [
+            Token::ASSIGN,
+            Token::PLUS,
+            Token::LPAREN,
+            Token::RPAREN,
+            Token::LBRACE,
+            Token::RBRACE,
+            Token::COMMA,
+            Token::SEMICOLON,
+        ];
+        let mut l = Lexer::new(input.to_string());
+
+        for (i, tt) in tests.iter().enumerate() {
+            let tok = l.next_token().unwrap();
+            assert_eq!(
+                tok, *tt,
+                "tests[{i}] - tokentype wrong. expected={tt:?}, got={tok:?}"
+            );
+        }
     }
 }
--- rust/interpreter/src/token.rs
+++ rust/interpreter/src/token.rs
@@ -1,1 +1,20 @@
+#[derive(PartialEq, Debug)]
+pub enum Token {
+    ILLEGAL,
+    EOF,
+    // Identifiers + literals
 
+    // Operators
+    ASSIGN,
+    PLUS,
+
+    // Delimiters
+    COMMA,
+    SEMICOLON,
+
+    LPAREN,
+    RPAREN,
+    LBRACE,
+    RBRACE,
+    // Keywords
+}
