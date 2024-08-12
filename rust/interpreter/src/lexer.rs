eec0f6c95c81a50657b80d56cb3febf81f5aacba
Dehan Jaco Lamprecht <dehanjl@pm.me>
2024-08-12 12:06:23 +0200

    Complete an improved lexer

--- rust/interpreter/src/lexer.rs
+++ rust/interpreter/src/lexer.rs
@@ -1,42 +1,36 @@
-use std::str::Chars;
+use crate::token::{lookup_ident, Token};
+use anyhow::Result;
 
-use crate::token::Token;
-
-struct Lexer<'a> {
-    input: String,
+struct Lexer {
+    input: Vec<u8>,
     position: usize,      // current position in input (points to current char)
     read_position: usize, // current reading position in input (after current char)
     ch: char,             // current char under examination
-    chars: Chars<'a>,
 }
 
-impl Lexer<'_> {
-    fn new(input: &str) -> Lexer<'_> {
+impl Lexer {
+    fn new(input: &str) -> Lexer {
         let mut l = Lexer {
-            input: input.to_string(),
+            input: input.to_string().into_bytes(),
             position: 0,
             read_position: 0,
             ch: '\0',
-            chars: input.chars(),
         };
         l.read_char();
         l
     }
 
     fn read_char(&mut self) {
-        self.ch = self.chars.next().unwrap_or('\0');
+        // TODO: see if I can make this not use to_owned
+        self.ch = self.input.get(self.read_position).unwrap_or(&0).to_owned() as char;
 
         self.position = self.read_position;
         self.read_position += 1;
     }
 
     fn peek_char(&self) -> char {
-        self.chars
-            .clone()
-            .peekable()
-            .peek()
-            .cloned()
-            .unwrap_or('\0')
+        // TODO: see if I can make this not use to_owned
+        self.input.get(self.read_position).unwrap_or(&0).to_owned() as char
     }
 
     fn skip_whitespace(&mut self) {
@@ -45,33 +39,67 @@ impl Lexer<'_> {
         }
     }
 
-    fn read_identifier(&mut self) -> String {
+    fn read_identifier(&mut self) -> Result<String> {
         let position = self.position;
         while self.ch.is_ascii_alphabetic() || self.ch == '_' {
             self.read_char();
         }
-        self.input[position..self.position].to_string()
+        Ok(String::from_utf8(
+            self.input[position..self.position].to_vec(),
+        )?)
     }
 
-    fn read_number(&mut self) -> String {
+    fn read_number(&mut self) -> Result<String> {
         let position = self.position;
         while self.ch.is_ascii_digit() {
             self.read_char();
         }
-        self.input[position..self.position].to_string()
+
+        Ok(String::from_utf8(
+            self.input[position..self.position].to_vec(),
+        )?)
     }
 
     fn next_token(&mut self) -> Option<Token> {
-        let tok = match self.ch {
-            '=' => Token::ASSIGN,
-            '+' => Token::PLUS,
-            '(' => Token::LPAREN,
-            ')' => Token::RPAREN,
-            '{' => Token::LBRACE,
-            '}' => Token::RBRACE,
-            ',' => Token::COMMA,
-            ';' => Token::SEMICOLON,
-            '\0' => return None,
+        self.skip_whitespace();
+        let tok = match (self.ch, self.peek_char()) {
+            ('=', '=') => {
+                self.read_char();
+                Token::EQ
+            }
+            ('=', _) => Token::ASSIGN,
+            ('!', '=') => {
+                self.read_char();
+                Token::NOT_EQ
+            }
+            ('!', _) => Token::BANG,
+            ('+', _) => Token::PLUS,
+            ('-', _) => Token::MINUS,
+            ('*', _) => Token::ASTERISK,
+            ('/', _) => Token::SLASH,
+            ('<', _) => Token::LT,
+            ('>', _) => Token::GT,
+            (',', _) => Token::COMMA,
+            (';', _) => Token::SEMICOLON,
+            ('(', _) => Token::LPAREN,
+            (')', _) => Token::RPAREN,
+            ('{', _) => Token::LBRACE,
+            ('}', _) => Token::RBRACE,
+            ('0'..='9', _) => {
+                return Some(if let Ok(num) = self.read_number() {
+                    Token::INT(num)
+                } else {
+                    Token::ILLEGAL
+                });
+            }
+            ('a'..='z', _) | ('A'..='Z', _) | ('_', _) => {
+                return Some(if let Ok(ident) = self.read_identifier() {
+                    lookup_ident(ident.as_str())
+                } else {
+                    Token::ILLEGAL
+                });
+            }
+            ('\0', _) => return None,
             _ => Token::ILLEGAL,
         };
         self.read_char();
@@ -79,9 +107,19 @@ impl Lexer<'_> {
     }
 }
 
+impl Iterator for Lexer {
+    type Item = Token;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next_token()
+    }
+}
+
 #[cfg(test)]
 mod tests {
+
     use super::*;
+
     #[test]
     fn test_next_token_simple() {
         let input = "=+(){},;";
@@ -98,11 +136,185 @@ mod tests {
         let mut l = Lexer::new(input);
 
         for (i, tt) in tests.iter().enumerate() {
-            let tok = l.next_token().unwrap();
+            let tok = l.next().unwrap();
             assert_eq!(
                 tok, *tt,
                 "tests[{i}] - tokentype wrong. expected={tt:?}, got={tok:?}"
             );
         }
+
+        assert_eq!(l.next(), None, "expected next to be None");
+    }
+
+    #[test]
+    fn test_next_token() {
+        let input = r#"let five = 5;
+let ten = 10;
+
+let add = fn(x, y) {
+  x + y;
+};
+
+let result = add(five, ten);
+"#;
+        let tests = [
+            Token::LET,
+            Token::IDENT("five".to_owned()),
+            Token::ASSIGN,
+            Token::INT("5".to_owned()),
+            Token::SEMICOLON,
+            Token::LET,
+            Token::IDENT("ten".to_owned()),
+            Token::ASSIGN,
+            Token::INT("10".to_owned()),
+            Token::SEMICOLON,
+            Token::LET,
+            Token::IDENT("add".to_owned()),
+            Token::ASSIGN,
+            Token::FUNCTION,
+            Token::LPAREN,
+            Token::IDENT("x".to_owned()),
+            Token::COMMA,
+            Token::IDENT("y".to_owned()),
+            Token::RPAREN,
+            Token::LBRACE,
+            Token::IDENT("x".to_owned()),
+            Token::PLUS,
+            Token::IDENT("y".to_owned()),
+            Token::SEMICOLON,
+            Token::RBRACE,
+            Token::SEMICOLON,
+            Token::LET,
+            Token::IDENT("result".to_owned()),
+            Token::ASSIGN,
+            Token::IDENT("add".to_owned()),
+            Token::LPAREN,
+            Token::IDENT("five".to_owned()),
+            Token::COMMA,
+            Token::IDENT("ten".to_owned()),
+            Token::RPAREN,
+            Token::SEMICOLON,
+        ];
+
+        let mut l = Lexer::new(input);
+        for (i, tt) in tests.iter().enumerate() {
+            let tok = l.next().unwrap();
+            assert_eq!(
+                tok, *tt,
+                "tests[{i}] - wrong token found. expected={tt:?}, got={tok:?}"
+            );
+        }
+
+        assert_eq!(l.next(), None, "expected next to be None");
+    }
+
+    #[test]
+    fn test_next_token_full() {
+        let input = r#"let five = 5;
+let ten = 10;
+
+let add = fn(x, y) {
+  x + y;
+};
+
+let result = add(five, ten);
+!-/*5;
+5 < 10 > 5;
+
+if (5 < 10) {
+	return true;
+} else {
+	return false;
+}
+
+10 == 10;
+10 != 9;
+"#;
+        let tests = [
+            Token::LET,
+            Token::IDENT("five".to_owned()),
+            Token::ASSIGN,
+            Token::INT("5".to_owned()),
+            Token::SEMICOLON,
+            Token::LET,
+            Token::IDENT("ten".to_owned()),
+            Token::ASSIGN,
+            Token::INT("10".to_owned()),
+            Token::SEMICOLON,
+            Token::LET,
+            Token::IDENT("add".to_owned()),
+            Token::ASSIGN,
+            Token::FUNCTION,
+            Token::LPAREN,
+            Token::IDENT("x".to_owned()),
+            Token::COMMA,
+            Token::IDENT("y".to_owned()),
+            Token::RPAREN,
+            Token::LBRACE,
+            Token::IDENT("x".to_owned()),
+            Token::PLUS,
+            Token::IDENT("y".to_owned()),
+            Token::SEMICOLON,
+            Token::RBRACE,
+            Token::SEMICOLON,
+            Token::LET,
+            Token::IDENT("result".to_owned()),
+            Token::ASSIGN,
+            Token::IDENT("add".to_owned()),
+            Token::LPAREN,
+            Token::IDENT("five".to_owned()),
+            Token::COMMA,
+            Token::IDENT("ten".to_owned()),
+            Token::RPAREN,
+            Token::SEMICOLON,
+            Token::BANG,
+            Token::MINUS,
+            Token::SLASH,
+            Token::ASTERISK,
+            Token::INT("5".to_owned()),
+            Token::SEMICOLON,
+            Token::INT("5".to_owned()),
+            Token::LT,
+            Token::INT("10".to_owned()),
+            Token::GT,
+            Token::INT("5".to_owned()),
+            Token::SEMICOLON,
+            Token::IF,
+            Token::LPAREN,
+            Token::INT("5".to_owned()),
+            Token::LT,
+            Token::INT("10".to_owned()),
+            Token::RPAREN,
+            Token::LBRACE,
+            Token::RETURN,
+            Token::TRUE,
+            Token::SEMICOLON,
+            Token::RBRACE,
+            Token::ELSE,
+            Token::LBRACE,
+            Token::RETURN,
+            Token::FALSE,
+            Token::SEMICOLON,
+            Token::RBRACE,
+            Token::INT("10".to_owned()),
+            Token::EQ,
+            Token::INT("10".to_owned()),
+            Token::SEMICOLON,
+            Token::INT("10".to_owned()),
+            Token::NOT_EQ,
+            Token::INT("9".to_owned()),
+            Token::SEMICOLON,
+        ];
+        let mut l = Lexer::new(input);
+
+        for (i, tt) in tests.iter().enumerate() {
+            let tok = l.next().unwrap();
+            assert_eq!(
+                tok, *tt,
+                "tests[{i}] - tokentype wrong. expected={tt:?}, got={tok:?}"
+            );
+        }
+
+        assert_eq!(l.next(), None, "expected next to be None");
     }
 }
--- rust/interpreter/src/token.rs
+++ rust/interpreter/src/token.rs
@@ -3,10 +3,22 @@ pub enum Token {
     ILLEGAL,
     EOF,
     // Identifiers + literals
+    IDENT(String), // add, foobar, x, y, ...
+    INT(String),   // 1343456
 
     // Operators
     ASSIGN,
+    BANG,
     PLUS,
+    MINUS,
+    ASTERISK,
+    SLASH,
+
+    LT,
+    GT,
+
+    EQ,
+    NOT_EQ,
 
     // Delimiters
     COMMA,
@@ -17,4 +29,26 @@ pub enum Token {
     LBRACE,
     RBRACE,
     // Keywords
+    FUNCTION,
+    LET,
+    TRUE,
+    FALSE,
+    IF,
+    ELSE,
+    RETURN,
+}
+
+/// Acts as a lookup table for keywords
+/// Returns the keyword if it exists, otherwise returns a `Token::IDENT`
+pub fn lookup_ident(ident: &str) -> Token {
+    match ident {
+        "fn" => Token::FUNCTION,
+        "let" => Token::LET,
+        "true" => Token::TRUE,
+        "false" => Token::FALSE,
+        "if" => Token::IF,
+        "else" => Token::ELSE,
+        "return" => Token::RETURN,
+        _ => Token::IDENT(ident.to_string()),
+    }
 }
