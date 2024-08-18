0c4647dc6ade38d65ae4d3ad0edcb4e14cea5c30
Dehan Jaco Lamprecht <dehanjl@pm.me>
2024-08-18 20:22:49 +0200

    First bit of Golang AST and Parser implementation

--- README.md
+++ README.md
@@ -4,9 +4,11 @@ My implementation of Thorsten Ball's Monkey language
 ## Progress
 [x] Chapter 1: Lexing (GO)
 [x] Chapter 1: Lexing (Rust)
+[/] Chapter 2: Parsing (GO)
 
 ## Future Features
 - Unicode support
 - Floating point number support
 - Loops
 - `and`, `or`, `not` operators
+- Comments
---
+++ golang/interpreter/ast/ast.go
@@ -0,0 +1,49 @@
+package ast
+
+import "monkey/token"
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+// Program is the root node of every AST that the parser produces. Implements the Node interface.
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+// LetStatement represents a let statement. Implements the Statement interface.
+type LetStatement struct {
+	Token token.Token // the token.LET token
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode()       {}
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
+// Indentifier represents an ident in a let statement. Implements the Expression interface.
+type Identifier struct {
+	Token token.Token // the token.IDENT token
+	Value string
+}
+
+func (i *Identifier) expressionNode()      {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
